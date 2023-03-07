7eca1fb92ebb6a67bc6bab9f022b914f0c5cb047
legion <abdukarimuz@mail.ru>
2023-03-07 21:24:52 +0500

    model done

---
+++ app/src/main/java/com/naa/uz/smstabriklar/model/SherModel.kt
@@ -0,0 +1,10 @@
+package com.naa.uz.smstabriklar.model
+
+data class SherModel(
+    var kategoriya: Int,
+    var title: String,
+    var body: String,
+    var liked: Boolean,
+    var yangilari: Boolean
+
+)
